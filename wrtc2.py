c5a0ccc7823a5da1315fed2d748cf61fb9638e90
SanjaySinha <sanjayk.sinha@nic.in>
2022-06-30 17:22:30 +0530

    Update wrtc2.py

--- wrtc2.py
+++ wrtc2.py
@@ -1,8 +1,36 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+#####
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-img_file_buffer = st.camera_input("Take a picture")
+#loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    img_file_buffer = st.camera_input("Take a picture")
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_LOCATION" in result:
+        st.write(result.get("GET_LOCATION"))
+######
+
+
+
+#img_file_buffer = st.camera_input("Take a picture")
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
