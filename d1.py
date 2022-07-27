03d78731b104349117f37ceaf74ab6a820cb7fe2
SanjaySinha <sanjayk.sinha@nic.in>
2022-07-27 11:10:49 +0530

    Update d1.py

--- d1.py
+++ d1.py
@@ -1,50 +1,24 @@
-from streamlit_elements import elements, mui, html
+with elements("style_elements_css"):
 
-# Create a frame where Elements widgets will be displayed.
-#
-# Elements widgets will not render outside of this frame.
-# Native Streamlit widgets will not render inside this frame.
-#
-# elements() takes a key as parameter.
-# This key can't be reused by another frame or Streamlit widget.
-
-with elements("new_element"):
-
-    # Let's create a Typography element with "Hello world" as children.
-    # The first step is to check Typography's documentation on MUI:
-    # https://mui.com/components/typography/
+    # For any other element, use the 'css' property.
     #
-    # Here is how you would write it in React JSX:
-    #
-    # <Typography>
-    #   Hello world
-    # </Typography>
-        mui.Typography("Hello world")
-with elements("multiple_children"):
+    # <div
+    #   css={{
+    #     backgroundColor: 'hotpink',
+    #     '&:hover': {
+    #         color: 'lightgreen'
+    #     }
+    #   }}
+    # >
+    #   This has a hotpink background
+    # </div>
 
-    # You have access to Material UI icons using: mui.icon.IconNameHere
-    #
-    # Multiple children can be added in a single element.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   Hello world
-    # </Button>
-
-        mui.Button(
-        mui.icon.EmojiPeople,
-        mui.icon.DoubleArrow,
-        "Button with multiple children"
+    html.div(
+        "This has a hotpink background",
+        css={
+            "backgroundColor": "hotpink",
+            "&:hover": {
+                "color": "lightgreen"
+            }
+        }
     )
-
-    # You can also add children to an element using a 'with' statement.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   <Typography>
-    #     Hello world
-    #   </Typography>
-    # </Button>
-
