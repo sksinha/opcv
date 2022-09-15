2b036f7660459e6b68efd12951a239716ee25c00
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-15 11:44:25 +0530

    Create gpt3.py

---
+++ gpt3.py
@@ -0,0 +1,10 @@
+import os
+import openai
+import json
+import streamlit as st
+import streamlit.components.v1 as components
+import requests
+import numpy as np 
+
+from io import BytesIO
+from time import sleep
