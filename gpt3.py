35f8916f3fcbc670c3dd88bc96653c004b0c25a1
SanjaySinha <sanjayk.sinha@nic.in>
2022-09-15 16:23:50 +0530

    Update gpt3.py

--- gpt3.py
+++ gpt3.py
@@ -1,6 +1,8 @@
 import os
 import openai
+from streamlit_card import card
 import json
+from streamlit_card import card
 import streamlit as st
 import streamlit.components.v1 as components
 import requests
@@ -9,3 +11,6 @@ import numpy as np
 from io import BytesIO
 from time import sleep
 assembly_auth_key = "AssemblyAI-Auth-Key"
+
+
+card(title="Hello World!", text="Some description", image="http://placekitten.com/200/300")
